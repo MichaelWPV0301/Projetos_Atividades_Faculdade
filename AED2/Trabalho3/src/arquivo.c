4fd8bd19bf53540c3931b9e03fbd2763510b2056
michael <michael.vieira@icomp.ufam.edu>
2025-06-04 11:46:07 -0400

    Criando a função dentro da main e o passo a passo em notas

--- AED2/Trabalho3/src/arquivo.c
+++ AED2/Trabalho3/src/arquivo.c
@@ -7,18 +7,6 @@
 
 #include <stdio.h>
 
-#define NOME_TAM 50
-#define CURSO_TAM 30
-
-// Definição da estrutura de registro
-typedef struct {
-    int matricula;             // chave primária (única)
-    char nome[NOME_TAM];       // nome do aluno
-    char curso[CURSO_TAM];     // curso (ex: Engenharia, Direito)
-    int idade;                 // usado nas buscas com <, >, etc.
-    float coeficiente;         // coeficiente de rendimento (CR)
-    int anoIngresso;           // ano de ingresso
-} Registro;
 
 // Função para abrir o arquivo
 FILE* abrirArquivo(const char* nomeArquivo, const char* modo) {
@@ -82,24 +70,4 @@ int obterNumeroRegistros(FILE* arquivo) {
     return tamanhoArquivo / sizeof(Registro); // Retorna o número de registros
 }
 
-int main() {
-    // Criando ou abrindo o arquivo em modo leitura e escrita binária
-    FILE *arquivo = abrirArquivo("cadastro_alunos.dat", "r+b");
 
-    // Adicionando um novo aluno
-    Registro aluno1 = {12345, "João Silva", "Engenharia", 20, 8.5, 2022};
-    adicionarRegistro(arquivo, &aluno1);
-
-    // Obter o número de registros
-    int numRegistros = obterNumeroRegistros(arquivo);
-    printf("Número de registros no arquivo: %d\n", numRegistros);
-
-    // Ler o primeiro registro (offset 0)
-    Registro alunoLido = lerRegistro(arquivo, 0);
-    printf("Matricula: %d, Nome: %s, Curso: %s, Idade: %d\n", alunoLido.matricula, alunoLido.nome, alunoLido.curso, alunoLido.idade);
-
-    // Fechar o arquivo
-    fecharArquivo(arquivo);
-
-    return 0;
-}
