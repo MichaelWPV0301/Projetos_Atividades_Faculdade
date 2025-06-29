1a7e4ddfb911175adad58570803bb371d20bdff2
Michael Willian <michael.vieira@icomp.ufam.edu.br>
2025-06-29 14:40:46 -0400

    Adicionei um arquivo invertLista2 para verificar se os códigos estão certos

---
+++ Organização de computadores/invertLista2.asm
@@ -0,0 +1,45 @@
+DATA r2, 0x01
+DATA r0, 0xf9
+
+
+DATA r1, 0x30
+ST r0,r1
+ADD r2,r0
+DATA r1, 0x29;fa
+ST r0,r1
+ADD r2,r0
+DATA r1, 0x28 ;fb
+ST r0,r1
+ADD r2,r0
+DATA r1, 0x27 ;fc
+ST r0,r1
+ADD r2,r0
+DATA r1, 0x26 ;fd
+ST r0,r1
+ADD r2,r0
+DATA r1, 0x25 ;fe
+ST r0,r1
+ADD r2,r0
+DATA r1, 0x24 ;ff
+ST r0,r1
+
+
+
+
+Data r0, 0xf9
+Data r2, 0xff
+loop:   CMP r0,r2 ;loop
+        JAE final ;final
+        LD r0,r1
+        LD r2,r3
+        ST r0,r3
+        ST r2,r1
+        DATA r1, 0x01
+        ADD r1,r0
+        DATA r3,0x01
+        NOT r1,r1
+        ADD r3,r1
+        ADD r1,r2
+        JMP loop ;loop
+        clf
+final: halt
