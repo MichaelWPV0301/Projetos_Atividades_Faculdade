e71ea44d75ec52b9f5bd3f34d627622144ff36f6
michael <michael.vieira@icomp.ufam.edu.br>
2025-07-04 00:42:44 -0400

    Terminei de aplicar as funções no montador, só precisa debugar possíveis erros

--- Organização de computadores/compilerv2 copy.py
+++ Organização de computadores/compilerv2 copy.py
@@ -71,8 +71,11 @@ def converte_registradores(ra, rb):
     return hexaregs
 
 
-def instru_com_regs(instrucao, ra, rb):
-    hexaregs = converte_registradores(ra, rb)
+def instru_com_regs(instrucao, ra=0, rb=0):
+    if(instrucao=="DATA"):
+        rb = ra
+        ra = 0
+    hexaregs = converte_registradores(ra.upper(), rb.upper())
     hexainstru = instrucoes1[instrucao] + hexaregs
     return hexainstru
 
@@ -118,7 +121,7 @@ def instru_sem_regs(instrucao):
         hexinstru = instru_jcaez(instrucao)
     return hexinstru
 
-def instru_outside(lista):
+def instru_outside(partes):
     hexa = outside["BASE"]  # Começa com BASE = 0x7
     comandos = outside[partes[0].upper()] + outside[partes[1].upper()] + registradores[partes[2].upper()]
     comando = int(comandos, 2)  # Monta comando binário total
@@ -126,8 +129,6 @@ def instru_outside(lista):
     hexa += comando[2:]  # Junta BASE + bits do comando
     return hexa
 
-def pseudo_instru(instrucao, ra=0, rb=0):
-    return "por fazer"
 
 # Abre o arquivo .asm de entrada
 with open(entrada) as arquivo:
@@ -137,156 +138,88 @@ with open(entrada) as arquivo:
         if not linha:
             continue  # Ignora linhas vazias
 
-        linha = linha.replace(",", " ")  # Substitui vírgulas por espaços para facilitar o split
-        partes = linha.strip().split(';')  # Remove os comentários
-        partes = partes[0].strip().split()  # Separa os elementos da instrução
+        partes = limpa_linhas(linha) # Separa os elementos da instrução
 
         if partes:  # Se houver instrução na linha
 
             # Verifica se há label no início da linha (ex: INICIO: ...)
-            if ":" in partes[0]:
-                label[partes[0]] = f"0x{endereco+1:02x}"  # Salva o endereço do label
-                partes = partes[1:]  # Remove o label da linha
+            partes = verifica_label(partes)
 
-            instrucao = partes[0].upper()  # Converte a instrução para maiúsculo
+            # Converte a instrução para maiúsculo
+            instrucao = partes[0].upper() 
 
             # INSTRUÇÕES COM DOIS REGISTRADORES (1 BYTE)
             if instrucao in instrucoes1:
-                reg1 = partes[1].upper()
-                reg2 = partes[2].upper()
-                regs = registradores[reg1] + registradores[reg2]  # Concatena os bits RA + RB
-                inteiro = int(regs, 2)  # Converte binário para inteiro
-                hexaregs = hex(inteiro)  # Converte inteiro para hexadecimal
-                hexa = instrucoes1[instrucao] + hexaregs[2:]  # Monta instrução final em hexa
+                hexainstru = instru_com_regs(instrucao, partes[1], partes[2])
+                instrucoes_hex.append(hexainstru)
+                endereco+=1
+                if(instrucao == "DATA"):
+                    hexainstru = partes[2]
+                    if(verifica_intervalo_data(hexainstru)):
+                        if("-" in hexainstru):
+                            hexainstru = complemento_de_dois(hexainstru)
+                        hexainstru = "0x" + converte_hexadecimal(hexainstru)
+                        instrucoes_hex.append(hexainstru)
+                        endereco+=1
+                    else:
+                        print("Erro no valor colocado no Data")
+                        break
 
+            
             # INSTRUÇÕES COM DOIS BYTES (JMPR, DATA, JMP, CLF, JCAEZ)
             elif instrucao in instrucoes2 or instrucao[0] == "J":
-
-                # DATA e JMPR possuem registrador + valor imediato
-                if instrucao == "DATA" or instrucao == "JMPR":
-                    reg = registradores[partes[1].upper()]  # Pega registrador RX
-                    inteiro = int(reg, 2)
-                    hexareg = hex(inteiro)
-                    hexa = instrucoes2[instrucao] + hexareg[2:]  # Primeiro byte (op + reg)
-
-                    if instrucao == "DATA":  # Para DATA, precisa armazenar o segundo byte (ADDR)
-                        instrucoes_hex.append(hexa)
-                        endereco += 1
-                        hexa = partes[2]  # Segundo operando (ADDR)
-
-                        try:
-                            if "x" not in hexa and "b" not in hexa:
-                                hexa = int(hexa)  # Trata decimal puro
-                                if hexa > 127 or hexa < -128:
-                                    print("Erro: valor fora do intervalo permitido (-128 a 127)")
-                                    break
-                            else:
-                                hexa = int(hexa, 0)  # Converte bin/hex str -> int
-                        except ValueError:
-                            print("Erro: valor inválido, não é um número inteiro")
-                            break
-
-                        if '-' in str(hexa):  # Se valor negativo, aplica complemento de dois
-                            hexa = f"{hexa:09b}"
-                            hexa = "".join("1" if b == "0" else "0" for b in hexa if b != '-')
-                            hexa = int(hexa, 2) + 1
-
-                        hexa = f"0x{hexa:02x}"  # Formata o valor final
-
-                # JMP e CLF usam opcode fixo + segundo byte com o endereço
-                elif instrucao == "JMP" or instrucao == "CLF":
-                    hexa = instrucoes2[instrucao]  # Opcode fixo da instrução
-                    if instrucao == "JMP":
-                        instrucoes_hex.append(hexa)
-                        endereco += 1
-                        hexa = partes[1]  # Segundo byte: endereço
-                        try:
-                            hexa = int(hexa, 0)
-                            if hexa < 0 or hexa > 255:
-                                print("Erro: valor fora do intervalo permitido (0 a 255)")
-                                break
-                            hexa = hex(hexa)
-                        except:
-                            instrucoes_hex.append(hexa)
-                            endereco += 1
-                            continue
-
-                # JCAEZ: instrução condicional com combinação de flags + endereço
-                elif instrucao[0] == "J":
-                    letras = list(instrucao[1:].upper())  # Pega os flags (CAEZ)
-                    letrasBin = sum(int(jcaez[letra], 2) for letra in letras)  # Soma os bits
-                    hexaLetras = hex(letrasBin)  # Converte para hexadecimal
-                    hexa = instrucoes2[instrucao[0]] + hexaLetras[2:]  # Monta primeiro byte
-
-                    instrucoes_hex.append(hexa)
-                    endereco += 1
-                    hexa = partes[1]  # Segundo byte (endereço de salto)
-                    try:
-                        hexa = int(hexa, 0)
-                        if hexa > 255 or hexa < 0:
-                            print("Erro: valor fora do intervalo permitido (0 a 255)")
-                            break
-                        hexa = f"0x{hexa:02x}"
-                    except:
-                        instrucoes_hex.append(hexa)
-                        endereco += 1
-                        continue
-
+                hexainstru = instru_sem_regs(instrucao)
+                instrucoes_hex.append(hexainstru)
+                endereco += 1
+                if(instrucao[0] == "J"):
+                    hexainstru = partes[2]
+                    if(verifica_intervalo_data(hexainstru)):
+                        instrucoes_hex.append(hexainstru)
+                        endereco+=1
+                    else:
+                        print("Endereço fora do intervalo de 0 e 255")
+                        break
+            
             # INSTRUÇÕES DE PERIFÉRICOS: IN/OUT/DATA/ADDR
             elif instrucao in outside:
-                hexa = outside["BASE"]  # Começa com BASE = 0x7
-                comandos = outside[partes[0].upper()] + outside[partes[1].upper()] + registradores[partes[2].upper()]
-                comando = int(comandos, 2)  # Monta comando binário total
-                comando = hex(comando)
-                hexa += comando[2:]  # Junta BASE + bits do comando
-
+                hexainstru = instru_outside(partes)
+                instrucoes_hex.append(hexainstru)
+                endereco+=1
             # INSTRUÇÕES ESPECIAIS (NÃO ESTÃO NOS SETS)/
             else:
                 # MOVE RX, RY: troca de valores via XOR Duplo
                 if instrucao == 'MOVE':
-                    reg1 = partes[1].upper()
-                    reg2 = partes[2].upper()
 
-                    regs = registradores[reg2] + registradores[reg1]  # XOR RY, RX
-                    inteiro = int(regs, 2)
-                    hexaregs = hex(inteiro)
-                    hexa = instrucoes1['XOR'] + hexaregs[2:]
-                    instrucoes_hex.append(hexa)
-                    endereco += 1
+                    hexainstru = instru_com_regs("XOR", partes[2], partes[1])
+                    instrucoes_hex.append(hexainstru)
 
-                    regs = registradores[reg1] + registradores[reg2]  # XOR RX, RY
-                    inteiro = int(regs, 2)
-                    hexaregs = hex(inteiro)
-                    hexa = instrucoes1['XOR'] + hexaregs[2:]
-                    instrucoes_hex.append(hexa)
-                    endereco += 1
+                    hexainstru = instru_com_regs("XOR", partes[1], partes[2])
+                    instrucoes_hex.append(hexainstru)
+                    endereco += 2
                     continue
 
                 # CLR RX: limpa registrador (gera 0x2X)
                 elif instrucao == 'CLR':
-                    reg = registradores[partes[1].upper()]
-                    inteiro = int(reg, 2)
-                    hexareg = hex(inteiro)
-                    hexa = "0x2" + hexareg[2:]
-                    instrucoes_hex.append(hexa)
-                    endereco += 1
-                    hexa = '0x00'
+                    hexainstru = instru_com_regs("DATA", partes[1])
+                    instrucoes_hex.append(hexainstru)
+                    instrucoes_hex.append("0x00")
+                    endereco += 2
+
+
 
                 # HALT: simula parada como JMP para próxima linha
                 elif instrucao == 'HALT':
-                    hexa = instrucoes2['JMP']
-                    instrucoes_hex.append(hexa)
+                    hexainstru = instru_sem_regs("JMP")
+                    instrucoes_hex.append(hexainstru)
                     endereco += 1
-                    if endereco <= 255:
+                    if(verifica_intervalo_data(endereco)):
                         instrucoes_hex.append(f"0x{endereco:02x}")
-                        endereco += 1
-                        continue
+                        endereco +=1
                     else:
                         print("Erro: valor fora do intervalo permitido (0 a 255)")
                         break
-            instrucoes_hex.append(hexa)  # Adiciona último byte da instrução
-            endereco += 1
-
+            print(f"Instrução: {partes}. hexadecimal final: {hexainstru}")
+            
 # Gera arquivo de saída no formato compatível com o Logisim
 print(instrucoes_hex)
 with open(saida, "w") as f:
